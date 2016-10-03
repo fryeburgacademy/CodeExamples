b9b5f4eef071835558941c6eaf4e8c5439d128aa
igloowhite <dave.sartory@gmail.com>
2016-10-02 22:41:27 -0400

    More basics

---
+++ Basic Examples/ArrayBasics.java
@@ -0,0 +1,41 @@
+
+public class ArrayBasics {
+    
+    public static void main(String[] args){
+        
+		// some basic instantiations
+		int[] example = new int[4];
+		String[] sExample = new String[7];
+		boolean[] bExample = new boolean[3];
+		Object[] oExample = new Object[2];
+		
+		
+		// create a new array of int values with 5 elements, no assigned values
+		int[] simpleArray = new int[5];
+		// values must then be filled in manually
+		simpleArray[0] = 14;
+		simpleArray[1] = 34;
+		simpleArray[2] = 756;
+		simpleArray[3] = 5;
+		simpleArray[4] = 149908;
+		
+		// create a new array of int values with 6 elements AND assign values
+		// use shorthand instantiation
+		int[] shorthandArray = { 32, 4545, 422234, 4, 678, 99};
+		
+		// display them using a method that loops through all values and prints
+		printArray(simpleArray);
+		printArray(shorthandArray);
+    }
+	
+	/**
+	* Print each value in the given array to the console
+	*/
+	public static void printArray(int[] a){
+		for(int i = 0; i < a.length; i++){
+			System.out.print(a[i] + "   ");
+		}
+		// print a line break
+		System.out.println();
+	}
+}
