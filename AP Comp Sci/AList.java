7472155ffd5e97cfc413678479f22e54fe3bc89e
igloowhite <dave.sartory@gmail.com>
2016-10-18 09:02:23 -0400

    More class stuff

---
+++ AP Comp Sci/AList.java
@@ -0,0 +1,30 @@
+import java.util.List;
+import java.util.ArrayList;
+/**
+* Basic ArrayList example
+**/
+public class AList{
+	
+	public static void main(String[] args){
+		
+		List<String> people = new ArrayList<String>();
+		
+		people.add("Dave");
+		people.add("Ken");
+		people.add("Bob");
+		people.add("Greg");
+		
+		people.remove("Jill");
+		people.add(1,"Curtis");
+		people.set(2,"Andy");
+		
+		for(String s : people){
+			System.out.println(s);
+		}
+			
+	}
+
+	
+	
+
+}
