bfe6886070b594fa1ee8d10dae6efda5480e9170
igloowhite <igloowhite@users.noreply.github.com>
2016-11-07 08:22:29 -0500

    Create 2DQuiz.java

---
+++ AP Comp Sci/2DQuiz.java
@@ -0,0 +1,34 @@
+
+public class 2DQuiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String[][] names = {
+                        {"ken","bob","joe"},
+                        {"jill","ann","bill"},
+                        {"kate","sarah","alice"},
+                        {"dave","dom","timmy"}};
+        
+        // ROW MAJOR
+        for(int row = 0; row < names.length; row++){    
+            for(int col = 0; col < names[0].length; col++){
+                // IN THIS SPOT
+                // we have access to a SINGLE location in the grid
+                System.out.print(names[row][col] + "  "); 
+            }
+        }
+        
+        // COLUMN MAJOR
+        for(int col = 0; col < names[0].length; col++){    
+            for(int row = 0; row < names.length; row++){
+                System.out.print(names[row][col] + "  ");
+            }
+        }
+    
+        
+    }
+    
+}
